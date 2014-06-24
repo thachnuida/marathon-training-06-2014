cc2cc0b1aace8e81eb7a6fc1c820e4edae0c7544
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2014-06-24 15:44:25 +0700

    Problem 1 & 2 Python

---
+++ thuyen.truong/Python/Problem2/problem2.py
@@ -0,0 +1,7 @@
+def char_freq(str):
+	dic = {}
+	for index in range(len(str)):
+		if dic.has_key(str[index]) == False:
+			dic[str[index]] = str.count(str[index])
+	print "Dictionary for string : \n",dic
+char_freq("abbabcbdbabdbdbabababcbcbab")
