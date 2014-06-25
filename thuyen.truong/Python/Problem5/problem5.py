d9e411521e0a64c7c8cef8b6a8c2d748f95c1443
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2014-06-25 10:28:14 +0700

    Problem 5 of Python's Exercise

---
+++ thuyen.truong/Python/Problem5/problem5.py
@@ -0,0 +1,24 @@
+def check_balanced(str):
+	num=0
+	for index in range(len(str)):
+		if str[index] == '[':
+			num +=1
+		elif str[index] == ']':
+			num -=1
+			if num<0:
+				return False
+	return True
+
+import random
+n = int(raw_input("Enter number of bracket you want: "))
+str = []
+while (n > 0):
+	str.append("]")
+	str.append("[")
+	n -=1
+random.shuffle(str)
+bracket = "".join(str)
+if check_balanced(bracket):
+	print bracket, " is OK!"
+else:
+	print bracket, " is not OK"
